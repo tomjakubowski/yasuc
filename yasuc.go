c430e673cca3f60bb68503c2c469d7f421049a77
Tom Jakubowski <tom@crystae.net>
2016-02-21 19:16:46 -0800

    use hex-encoded sha256 of content for keys

--- yasuc.go
+++ yasuc.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-var (
-	pastes map[string]string
-	count  int
-)
+var pastes map[string]string
 
 const baseUrl = "http://localhost:8080"
+
+func stashPaste(paste string) string {
+	rawKey := sha256.Sum256([]byte(paste))
+	key := hex.EncodeToString(rawKey[:])
+	// TODO: launch nukes on collision
+	pastes[key] = paste
+	return key
+}
 
 func handleRoot(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Path != "/" {
@@ -28,9 +35,8 @@ func handleRoot(w http.ResponseWriter, req *http.Request) {
 		body := req.FormValue("sprunge")
 		// sprunge.us proceeds if there's no form data in 'sprunge', and just makes
 		// an empty paste.
-		pastes[fmt.Sprintf("%d", count)] = body
-		fmt.Fprintf(w, "%s/%d\n", baseUrl, count)
-		count++
+		key := stashPaste(body)
+		fmt.Fprintf(w, "%s/%s\n", baseUrl, key)
 		return
 	}
 	fmt.Fprintf(w, "usage message goes here\n")
