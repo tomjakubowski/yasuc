a4b738ff3f7f62c2aafada43859f78cb7bd01724
Tom Jakubowski <tom@crystae.net>
2016-02-27 14:14:07 -0800

    add license, usage message

--- yasuc.go
+++ yasuc.go
@@ -1,3 +1,20 @@
+// yasuc - yet another sprunge.us clone (command line pastebin)
+//
+// Copyright (C) Tom Jakubowski <tom@crystae.net>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -7,6 +24,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/boltdb/bolt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -17,6 +35,46 @@ const (
 	pastesBucket = "pastes"
 	maxPasteSize = 4 * 1024 * 1024
 )
+
+const usageText = `
+<!doctype html>
+<html>
+  <head>
+  </head>
+  <body>
+    <pre>
+yasuc(1)                          YASUC                          yasuc(1)
+
+NAME
+    yasuc - command line pastebin.
+
+SYNOPSIS
+    &lt;command&gt; | curl -F 'sprunge=&lt;-' {{.BaseURL}}
+
+DESCRIPTION
+    A command line pastebin.  Pastes are immutable and created with simple HTTP
+    POST requests. The path of a paste's URL is the SHA-256 digest of the
+    paste's contents.
+
+EXAMPLE
+    $ echo 'hello world' | curl -F 'sprunge=&lt;-' {{.BaseURL}}
+       {{.BaseURL}}/a948904f2f0f479b8f8197694b30184b0d2ed1c1cd2a1ec0fb85d299a192a447
+    $ firefox {{.BaseURL}}/a948904f2f0f479b8f8197694b30184b0d2ed1c1cd2a1ec0fb85d299a192a447
+
+COPYRIGHT
+    Copyright © Tom Jakubowski.  License AGPLv3: GNU Affero GPL Version 3
+    <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">https://www.gnu.org/licenses/agpl-3.0.en.html</a>.
+
+    This is free software; you are free to change and redistribute it.  There
+    is NO WARRANTY, to the extend permitted by law.  For copying conditions and
+    source, see <a href="https://github.com/tomjakubowski/yasuc">https://github.com/tomjakubowski/yasuc</a>.
+
+SEE ALSO
+    <a href="https://github.com/tomjakubowski/yasuc">http://github.com/tomjakubowski/yasuc</a>
+    </pre>
+  </body>
+</html>
+`
 
 type pasteTooLarge struct{}
 
@@ -118,7 +176,15 @@ func (h *handler) alles(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "%s/%s\n", baseURL, key)
 		return
 	}
-	fmt.Fprintf(w, "usage message goes here\n")
+	// usage message
+	tmpl, err := template.New("usage").Parse(usageText)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	baseURL := fmt.Sprintf("http://%s", req.Host)
+	data := struct{ BaseURL string }{baseURL}
+	_ = tmpl.Execute(w, data)
 }
 
 func newHandler(db *bolt.DB) http.Handler {
@@ -143,14 +209,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer func() { _ = db.Close() }()
 	err = db.Update(func(tx *bolt.Tx) error {
 		// make all of ze buckets
-		tx.CreateBucketIfNotExists([]byte(pastesBucket))
+		_, err := tx.CreateBucketIfNotExists([]byte(pastesBucket))
+		if err != nil {
+			_ = db.Close()
+			log.Fatal(err)
+		}
 		return nil
 	})
 	if err != nil {
-		db.Close() // deferred functions aren't called for fatal logs :|
+		_ = db.Close() // deferred functions aren't called for fatal logs :|
 		log.Fatal(err)
 	}
 
